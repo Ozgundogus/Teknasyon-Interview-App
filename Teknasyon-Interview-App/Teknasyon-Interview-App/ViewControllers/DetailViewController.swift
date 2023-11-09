d98bc2a8ee3087967047cf2b7515f23733b4370a
Ozgundogus <ozgundogus.can@ogr.iuc.edu.tr>
2023-11-09 21:41:02 +0300

    Setup project and created pages
    --> Storyboard removed
    --> Created Home and Detail View Controllers

---
+++ Teknasyon-Interview-App/Teknasyon-Interview-App/ViewControllers/DetailViewController.swift
@@ -0,0 +1,21 @@
+//
+//  DetailViewController.swift
+//  Teknasyon-Interview-App
+//
+//  Created by Ozgun Dogus on 9.11.2023.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+   
+
+}
